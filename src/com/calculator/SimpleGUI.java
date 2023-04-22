0262b17513f2fe0e8b9a5214e667f455f67090e3
KartashovStanislav <kcm281997@gmail.com>
2023-04-22 12:40:40 +0500

    rebuild project

--- src/com/calculator/SimpleGUI.java
+++ src/com/calculator/SimpleGUI.java
@@ -7,13 +7,18 @@ public class SimpleGUI extends JFrame{
     private JButton button = new JButton("Press");
     private JTextField input = new JTextField("", 5);
     private JLabel label = new JLabel("Input:");
-    private JRadioButton radioButton1 = new JRadioButton("Select");
-    private JRadioButton radioButton2 = new JRadioButton("Select");
+    private JRadioButton radioButton1 = new JRadioButton("addition");
+    private JRadioButton radioButton2 = new JRadioButton("multiplying");
+    private JRadioButton radioButton3 = new JRadioButton("subtraction");
+    private JRadioButton radioButton4 = new JRadioButton("division");
     private JCheckBox checkBox = new JCheckBox("Check", false);
+    private int number = 1;
 
     public SimpleGUI(){
         super("Simple Example");
-        this.setBounds(100, 100, 250,100);
+        Dimension screenPar = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setBounds((screenPar.width  - screenPar.height / 4) / 2, (screenPar.height  - screenPar.height / 4)/ 2,
+                screenPar.width / 4,screenPar.height / 4);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container container = this.getContentPane();
@@ -24,21 +29,74 @@ public class SimpleGUI extends JFrame{
         ButtonGroup buttonGroup = new ButtonGroup();
         buttonGroup.add(radioButton1);
         buttonGroup.add(radioButton2);
+        buttonGroup.add(radioButton3);
+        buttonGroup.add(radioButton4);
 
-        container.add(radioButton1);
-        radioButton1.setSelected(true);
-        container.add(radioButton2);
+        if (number != 0){
+            container.add(radioButton1);
+            container.add(radioButton2);
+            container.add(radioButton3);
+            container.add(radioButton4);
+        }
 
         button.addActionListener(new ButtonEventListener ());
-        container.add(checkBox);
+        container.add(button);
+    }
+    public SimpleGUI(int number){
+        super("Simple Example");
+        this.number = number;
+        Dimension screenPar = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setBounds((screenPar.width  - screenPar.height / 4) / 2, (screenPar.height  - screenPar.height / 4)/ 2,
+                screenPar.width / 4,screenPar.height / 4);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container container = this.getContentPane();
+        container.setLayout(new GridLayout(3,2,2,2));
+        container.add(label);
+        container.add(input);
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(radioButton1);
+        buttonGroup.add(radioButton2);
+        buttonGroup.add(radioButton3);
+        buttonGroup.add(radioButton4);
+
+        if (number != 0){
+            container.add(radioButton1);
+            container.add(radioButton2);
+            container.add(radioButton3);
+            container.add(radioButton4);
+        }
+
+        button.addActionListener(new ButtonEventListener ());
         container.add(button);
     }
     class ButtonEventListener implements ActionListener{
+
         public void actionPerformed (ActionEvent e){
             String message = "";
-            message += "Button was pressed\n Text is " + input.getText() + "\n";
-            message += (radioButton1.isSelected() ? "Radio1 is " : "Radio2 is " + "selected");
-            message += "\n" + "Checkbox is " + (checkBox.isSelected()? "checked" : "unchecked");
+            if (number == 0){
+                number = Integer.parseInt(input.getText());
+                new SimpleGUI(number);
+            }
+            else {
+                if (radioButton1.isSelected()){
+                    message += number + Integer.parseInt(input.getText());
+                    number = number + Integer.parseInt(input.getText());
+                }
+                else if (radioButton2.isSelected()){
+                    message += number * Integer.parseInt(input.getText());
+                    number = number * Integer.parseInt(input.getText());
+                }
+                else if (radioButton3.isSelected()){
+                    message += number - Integer.parseInt(input.getText());
+                    number = number - Integer.parseInt(input.getText());
+                }
+                else {
+                    message += number / Integer.parseInt(input.getText());
+                    number = number / Integer.parseInt(input.getText());
+                }
+            }
             JOptionPane.showMessageDialog(null, message, "output", JOptionPane.PLAIN_MESSAGE);
         }
     }
